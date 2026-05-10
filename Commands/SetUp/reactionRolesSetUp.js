39c624ac9853d3b65808b7ef5b0e5fd7ef84c492
dxxrkgxntel <sicarios.ot.oficial@gmail.com>
2026-05-10 02:12:44 +0200

    cambio de carpeta

--- Commands/SetUp/reactionRolesSetUp.js
+++ Commands/SetUp/reactionRolesSetUp.js
@@ -6,7 +6,7 @@ module.exports = {
     data:
         new SlashCommandBuilder()
 
-            .setName("rr-panel")
+            .setName("rr-setup")
 
             .setDescription(
                 "Crear panel de reaction roles"
