9b8c1940e0d3e6c891dec6cce7fa91f61b2a4203
dxxrkgxntel <sicarios.ot.oficial@gmail.com>
2026-05-11 03:59:41 +0200

    sistema de rank mejorado

--- Commands/Levels/perfil.js
+++ Commands/Levels/perfil.js
@@ -6,6 +6,9 @@ const {
 const Level =
     require("../../Models/Level");
 
+const EconomyUser =
+    require("../../Models/EconomyUser");
+
 module.exports = {
 
     data:
@@ -39,18 +42,33 @@ module.exports = {
         //////////////////////////////////////////////////////
 
         const user =
+
             interaction.options.getUser(
                 "usuario"
-            ) || interaction.user;
+            ) ||
+
+            interaction.user;
 
         //////////////////////////////////////////////////////
-        // DATA
+        // MEMBER
+        //////////////////////////////////////////////////////
+
+        const member =
+
+            await interaction.guild.members
+                .fetch(user.id)
+                .catch(() => null);
+
+        //////////////////////////////////////////////////////
+        // LEVEL DATA
         //////////////////////////////////////////////////////
 
         const data =
+
             await Level.findOne({
 
-                userId: user.id,
+                userId:
+                    user.id,
 
                 guildId:
                     interaction.guild.id
@@ -70,10 +88,38 @@ module.exports = {
         }
 
         //////////////////////////////////////////////////////
-        // LEADERBOARD POSITION
+        // ECONOMY
+        //////////////////////////////////////////////////////
+
+        let economy =
+
+            await EconomyUser.findOne({
+
+                guildId:
+                    interaction.guild.id,
+
+                userId:
+                    user.id
+            });
+
+        //////////////////////////////////////////////////////
+
+        if (!economy) {
+
+            economy = {
+
+                wallet: 0,
+
+                bank: 0
+            };
+        }
+
+        //////////////////////////////////////////////////////
+        // LEADERBOARD
         //////////////////////////////////////////////////////
 
         const leaderboard =
+
             await Level.find({
 
                 guildId:
@@ -81,16 +127,20 @@ module.exports = {
             })
 
                 .sort({
+
                     level: -1,
+
                     xp: -1
                 });
 
         //////////////////////////////////////////////////////
 
         const position =
+
             leaderboard.findIndex(
 
-                u => u.userId === user.id
+                u =>
+                    u.userId === user.id
             ) + 1;
 
         //////////////////////////////////////////////////////
@@ -98,10 +148,15 @@ module.exports = {
         //////////////////////////////////////////////////////
 
         const xpNeeded =
+
             (data.level + 1) * 100;
 
+        //////////////////////////////////////////////////////
+
         const progress =
+
             Math.floor(
+
                 (data.xp / xpNeeded) * 100
             );
 
@@ -110,10 +165,15 @@ module.exports = {
         //////////////////////////////////////////////////////
 
         const filled =
+
             Math.floor(progress / 10);
+
+        //////////////////////////////////////////////////////
 
         const empty =
             10 - filled;
+
+        //////////////////////////////////////////////////////
 
         const progressBar =
 
@@ -121,126 +181,289 @@ module.exports = {
             "⬛".repeat(empty);
 
         //////////////////////////////////////////////////////
-        // MEMBER
-        //////////////////////////////////////////////////////
-
-        const member =
-            await interaction.guild.members
-                .fetch(user.id)
-                .catch(() => null);
-
-        //////////////////////////////////////////////////////
         // BADGES
         //////////////////////////////////////////////////////
 
         let badges = "";
 
-        ////////////////////////////////////////////
+        //////////////////////////////////////////////////////
         // OWNER
-        ////////////////////////////////////////////
+        //////////////////////////////////////////////////////
 
         if (
+
             interaction.guild.ownerId ===
             user.id
+
         ) {
 
             badges += "👑 ";
         }
 
-        ////////////////////////////////////////////
+        //////////////////////////////////////////////////////
         // ADMIN
-        ////////////////////////////////////////////
+        //////////////////////////////////////////////////////
 
         if (
+
             member?.permissions.has(
                 "Administrator"
             )
+
         ) {
 
-            badges += "🛠 ";
+            badges += "🛠️ ";
         }
 
-        ////////////////////////////////////////////
+        //////////////////////////////////////////////////////
         // BOOSTER
-        ////////////////////////////////////////////
+        //////////////////////////////////////////////////////
 
         if (
+
             member?.premiumSince
+
         ) {
 
             badges += "🚀 ";
         }
 
-        ////////////////////////////////////////////
+        //////////////////////////////////////////////////////
         // TOPS
-        ////////////////////////////////////////////
+        //////////////////////////////////////////////////////
 
         if (position === 1)
+
             badges += "🥇 ";
 
         else if (position === 2)
+
             badges += "🥈 ";
 
         else if (position === 3)
+
             badges += "🥉 ";
+
+        //////////////////////////////////////////////////////
+        // RICH
+        //////////////////////////////////////////////////////
+
+        if (
+
+            economy.wallet >= 100000
+
+        ) {
+
+            badges += "💰 ";
+        }
+
+        //////////////////////////////////////////////////////
+        // HIGH LEVEL
+        //////////////////////////////////////////////////////
+
+        if (
+
+            data.level >= 25
+
+        ) {
+
+            badges += "🔥 ";
+        }
+
+        //////////////////////////////////////////////////////
+        // OLD ACCOUNT
+        //////////////////////////////////////////////////////
+
+        const oldAccount =
+
+            Date.now() -
+            user.createdTimestamp;
+
+        //////////////////////////////////////////////////////
+
+        const oneYear =
+
+            365 * 24 * 60 * 60 * 1000;
+
+        //////////////////////////////////////////////////////
+
+        if (
+
+            oldAccount >= oneYear
+
+        ) {
+
+            badges += "📜 ";
+        }
 
         //////////////////////////////////////////////////////
         // ACCOUNT DATE
         //////////////////////////////////////////////////////
 
         const created =
+
             `<t:${parseInt(
+
                 user.createdTimestamp / 1000
+
             )}:R>`;
+
+        //////////////////////////////////////////////////////
+        // JOIN DATE
+        //////////////////////////////////////////////////////
+
+        const joined =
+
+            member?.joinedTimestamp
+
+                ?
+
+                `<t:${parseInt(
+
+                    member.joinedTimestamp / 1000
+
+                )}:R>`
+
+                :
+
+                "Desconocido";
+
+        //////////////////////////////////////////////////////
+        // STATUS
+        //////////////////////////////////////////////////////
+
+        let status =
+            "🌙 Ausente";
+
+        //////////////////////////////////////////////////////
+
+        if (
+            member?.presence?.status ===
+            "online"
+        ) {
+
+            status =
+                "🟢 En línea";
+        }
+
+        //////////////////////////////////////////////////////
+
+        else if (
+
+            member?.presence?.status ===
+            "idle"
+
+        ) {
+
+            status =
+                "🌙 Ausente";
+        }
+
+        //////////////////////////////////////////////////////
+
+        else if (
+
+            member?.presence?.status ===
+            "dnd"
+
+        ) {
+
+            status =
+                "⛔ No molestar";
+        }
 
         //////////////////////////////////////////////////////
         // EMBED
         //////////////////////////////////////////////////////
 
         const embed =
+
             new EmbedBuilder()
 
                 .setColor("#8A2BE2")
 
                 .setAuthor({
 
-    name:
-        `${member?.displayName || user.username} | Perfil Premium`,
+                    name:
 
-    iconURL:
-        user.displayAvatarURL({
-            dynamic: true
-        })
-})
+                        `${member?.displayName || user.username} | Perfil Premium`,
+
+                    iconURL:
+
+                        user.displayAvatarURL({
+
+                            dynamic: true
+                        })
+                })
 
                 .setThumbnail(
+
                     user.displayAvatarURL({
+
                         dynamic: true,
+
                         size: 1024
                     })
+                )
+
+                .setImage(
+                    "https://media.discordapp.net/attachments/1499375657103392839/1501666280174915584/banner_bot.png?ex=6a0032f4&is=69fee174&hm=54a509859dcee24cd6a637b9e0373e1821b6ab3898eccd77a59591b6e6d55e3a&=&format=webp&quality=lossless&width=1288&height=515"
                 )
 
                 .setDescription(
 
                     `## 👤 ${member?.displayName || user.username}\n` +
-                    
-                    `${badges || "Sin badges"}\n\n` +
 
-                    `🏆 **Rank:** #${position}\n` +
+                    `> **Insignias:** ${badges || "Sin badges"}\n\n` +
 
-                    `⭐ **Nivel:** ${data.level}\n` +
+                    `> 🌐 Estado: **${status}**\n` +
 
-                    `✨ **XP:** ${data.xp}/${xpNeeded}\n` +
+                    `> 🏆 Rank Global: **#${position}**\n` +
 
-                    `📈 ${progressBar} **${progress}%**\n\n` +
+                    `> ⭐ Nivel: **${data.level}**\n` +
 
-                    `📅 **Cuenta creada:** ${created}`
+                    `> ✨ XP: **${data.xp}/${xpNeeded}**\n` +
+
+                    `> 📈 Progreso: **${progress}%**\n\n` +
+
+                    `${progressBar}\n`
+                )
+
+                .addFields(
+
+                    {
+
+                        name:
+                            "💰 Economía",
+
+                        value:
+
+                            `> 👛 Wallet: **${economy.wallet.toLocaleString()}**\n` +
+
+                            `> 🏦 Banco: **${economy.bank.toLocaleString()}**`,
+
+                        inline: true
+                    },
+
+                    {
+
+                        name:
+                            "📅 Información",
+
+                        value:
+
+                            `> 🗓️ Cuenta creada: ${created}\n` +
+
+                            `> 🚪 Entró al servidor: ${joined}`,
+
+                        inline: true
+                    }
                 )
 
                 .setFooter({
 
                     text:
-                        "Sistema de perfiles avanzado"
+                        "Bryant's Oficial • Sistema de perfiles avanzado"
                 })
 
                 .setTimestamp();
